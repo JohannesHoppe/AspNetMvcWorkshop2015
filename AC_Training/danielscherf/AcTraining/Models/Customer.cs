c14e8673fd46e44f823b5a51d6b4650384a70556
danielscherf <daniel.scherf.de@gmail.com>
2015-01-12 14:28:25 +0100

    werwer

---
+++ AC_Training/danielscherf/AcTraining/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AcTraining.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mail { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
