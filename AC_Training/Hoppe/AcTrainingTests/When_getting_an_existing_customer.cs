18da57df67692a0cde492ecbad72f88e54c3ba82
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-01-13 00:15:37 +0100

    simpler MSpec Tests (demo)

---
+++ AC_Training/Hoppe/AcTrainingTests/When_getting_an_existing_customer.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Machine.Specifications;
+
+namespace AcTrainingTests
+{
+    [Subject("Test")]
+    public class When_doing_a_simple_test
+    {
+        static int value1;
+        static int value2;
+        static int result;
+
+        private Establish context = () =>
+                                    {
+                                        value1 = 1;
+                                        value2 = 2;
+                                    };
+
+        Because of = () => result = value1 + value2;
+
+        It should_have_the_expected_result = () => result.Should().Be(3);
+    }
+}
