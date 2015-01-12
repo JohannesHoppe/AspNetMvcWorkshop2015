9b2ef8ae3bbddc7ac0412e83a80e0ff7634c5223
danielscherf <daniel.scherf.de@gmail.com>
2015-01-12 11:07:37 +0100

    demo project

---
+++ AC_Training/danielscherf/AcTraining/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AcTraining.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
