140de669023cc48073bc7360e686f8638ed7fc00
flo77 <florian.benger@gmx.de>
2015-01-12 14:32:33 +0100

    Changed

---
+++ AC_Training/FB/AcTraining/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AcTraining.Models
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
