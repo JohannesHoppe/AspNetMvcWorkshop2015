40be75c51a0537254cf477bf7783126b92a247cd
ClausK <Claus.Kessel@gmail.com>
2015-01-12 14:28:45 +0100

    neuer Versuch

---
+++ AC_Training/CK/AcTraining/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcTraining.Models
+{
+    public class Customer
+    {
+        public Customer()
+        {
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mail { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+
+
+    }
+}
