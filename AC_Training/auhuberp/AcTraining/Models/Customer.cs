9485e30988003854359778a3f91e8672d643c921
auhuberp <petra.auhuber@t-online.de>
2015-01-12 14:33:44 +0100

    sync

---
+++ AC_Training/auhuberp/AcTraining/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AcTraining.Models
+{
+	public class Customer
+	{
+		public Customer()
+		{
+
+		}
+
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Mail { get; set; }
+		public string Phone { get; set; }
+		public DateTime DateOfBirth { get; set; }
+	}
+}
