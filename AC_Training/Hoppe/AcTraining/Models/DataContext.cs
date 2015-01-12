c2de293ac511c9de334d63916db09aa5a3dde4a1
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-01-12 15:34:30 +0100

    using Effort instead of SQL database

--- AC_Training/Hoppe/AcTraining/Models/DataContext.cs
+++ AC_Training/Hoppe/AcTraining/Models/DataContext.cs
@@ -1,9 +1,16 @@
-﻿using System.Data.Entity;
+﻿using System.Data.Common;
+using System.Data.Entity;
 
 namespace AcTraining.Models
 {
     public class DataContext: DbContext 
     {
+        public DataContext(DbConnection connection) : base(connection, true)
+        {
+            
+        }
+
         public DbSet<Customer> Customers { get; set; }
+
     }
 }
