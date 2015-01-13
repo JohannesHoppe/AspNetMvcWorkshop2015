21a57ca928f0928643c962fc87a1338d42b29933
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-01-13 21:11:03 +0100

    example: CattyController with cache-control

--- AC_Training/Hoppe/AcTraining/Controllers/CattyController.cs
+++ AC_Training/Hoppe/AcTraining/Controllers/CattyController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Hosting;
 using System.Web.Http;
 
 namespace AcTraining.Controllers
@@ -11,13 +16,16 @@ namespace AcTraining.Controllers
     {
         public HttpResponseMessage GetImage()
         {
-            var msg = new HttpResponseMessage
-                      {
-                          Content = new StringContent("ich bin ein Bild")
-                      };
-            msg.Content.Headers.Expires = DateTime.UtcNow.AddDays(7);
+            byte[] bytes = File.ReadAllBytes(HostingEnvironment.MapPath("~/Content/cat.jpg"));
+            var response = new HttpResponseMessage(HttpStatusCode.OK) { Content = new ByteArrayContent(bytes) };
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+            response.Headers.CacheControl = new CacheControlHeaderValue
+                {
+                    Public = true,
+                    MaxAge = TimeSpan.FromSeconds(24 * 7 * 60)
+                };
 
-            return msg;
+            return response;
         }
     }
 }
