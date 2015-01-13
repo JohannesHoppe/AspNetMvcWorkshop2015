8d15f5c39d198eb6a3ea6303a20c7c749ee8eca9
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-01-13 21:03:31 +0100

    fixing my own odata installation

--- AC_Training/Hoppe/AcTraining/App_Start/WebApiConfig.cs
+++ AC_Training/Hoppe/AcTraining/App_Start/WebApiConfig.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.OData.Builder;
+using System.Web.Http.OData.Extensions;
+using AcTraining.Models;
 using Newtonsoft.Json.Serialization;
 
 namespace AcTraining
@@ -18,7 +21,12 @@ namespace AcTraining
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
-            );            
+            );
+
+            // OData
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Customer>("Customers");
+            config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
         }
     }
 }
--- AC_Training/Hoppe/AcTraining/Controllers/CustomersController.cs
+++ AC_Training/Hoppe/AcTraining/Controllers/CustomersController.cs
@@ -2,11 +2,12 @@
 using System.Net;
 using System.Web.Http;
 using System.Web.Http.Description;
+using System.Web.Http.OData;
 using AcTraining.Models;
 
 namespace AcTraining.Controllers
 {
-    public class CustomersController : ApiController
+    public class CustomersController : ODataController
     {
         private readonly ICustomerRepository _customerRep;
 
