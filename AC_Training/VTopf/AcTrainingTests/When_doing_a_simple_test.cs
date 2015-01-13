1494caea8088959c4d8e995f4bed446abc537b47
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-01-13 13:42:20 +0100

    my version of the tests

---
+++ AC_Training/VTopf/AcTrainingTests/When_doing_a_simple_test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcTrainingTests
+{
+    using FluentAssertions;
+    using Machine.Specifications;
+
+    [Subject("DaxDelister")]
+    public class When_we_have_an_index_product
+    {
+        static int value1;
+        static int value2;
+        static int result;
+
+        Establish context = () =>
+        {
+            value1 = 1;   // hole Produkt
+            value2 = 2;
+        };
+
+        Because of = () => result = value1 + value2;    // hier datum erreicht
+
+        It should_be_deleted_automatically = () => result.Should().Be(3);
+
+        public It should_be_deleted_automatically2 = () => result.Should().Be(3);
+    }
+}
