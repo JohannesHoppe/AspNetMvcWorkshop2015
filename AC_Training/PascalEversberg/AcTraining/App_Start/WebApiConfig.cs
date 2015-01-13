3d9af59e3cb45772c3748941fdf8bd1d9bbe909d
PascalEversberg <pascal.eversberg@gmx.de>
2015-01-13 17:26:07 +0100

    Tag 2 Ende

--- AC_Training/PascalEversberg/AcTraining/App_Start/WebApiConfig.cs
+++ AC_Training/PascalEversberg/AcTraining/App_Start/WebApiConfig.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
+using System.Web;
 using System.Web.Http;
-using Newtonsoft.Json.Serialization;
+using System.Web.Http.OData.Builder;
+using System.Web.Http.OData.Extensions;
+using AcTraining.Models;
 
 namespace AcTraining
 {
@@ -17,8 +19,14 @@ namespace AcTraining
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+                defaults: new {id = RouteParameter.Optional}
+                );
+
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Customer>("Customers");
+
+            config.Routes.MapODataServiceRoute("odata","odata", builder.GetEdmModel());
         }
+
     }
 }
