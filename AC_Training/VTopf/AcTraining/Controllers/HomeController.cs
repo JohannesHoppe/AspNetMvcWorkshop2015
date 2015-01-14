4a87617015ae92865a01ec67a88665626a2cf486
Verena Topf <verena.80@gmx.de>
2015-01-14 14:59:03 +0100

    Grid

--- AC_Training/VTopf/AcTraining/Controllers/HomeController.cs
+++ AC_Training/VTopf/AcTraining/Controllers/HomeController.cs
@@ -30,5 +30,10 @@ namespace AcTraining.Controllers
             var customer = rep.GetCustomer(1);
             return View(customer);
         }
+
+        public ActionResult Grid()
+        {
+            return View();
+        }
     }
 }
