22a9e690e8dcf89cb107670c6177ea5557a1f893
auhuberp <petra.auhuber@t-online.de>
2015-01-14 16:51:43 +0100

    Kendo Grid and Chart

--- AC_Training/auhuberp/AcTraining/Models/Customer.cs
+++ AC_Training/auhuberp/AcTraining/Models/Customer.cs
@@ -13,6 +13,7 @@ namespace AcTraining.Models
 		//public DateTime DateOfBirth { get; set; }
 		public int AmountOfOrders { get; set; }
 		public int AmountOfInvoices { get; set; }
+		public DateTime DateOfBirth { get; set; }
 
 		//public virtual List<Customer> Children { get; set; }
 	}
