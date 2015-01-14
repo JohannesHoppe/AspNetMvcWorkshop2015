3518710de6562d237966051bb8332fb267383ab1
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-01-14 11:16:59 +0100

    new resetcontroller

--- AC_Training/Hoppe/AcTraining/Controllers/ResetController.cs
+++ AC_Training/Hoppe/AcTraining/Controllers/ResetController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using AcTraining.Models;
 using AutoPoco;
@@ -32,7 +33,10 @@ namespace AcTraining.Controllers
 
             db.SaveChanges();
 
-            var response = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent("Demo Data was resetted!") };
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+                           {
+                               Content = new StringContent("Demo Data was resetted!")
+                           };
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
             return response;
         }
