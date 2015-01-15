784de8b304ef5c598c24af582a364ac88d2c9e4b
ArminRothaupt <rotsonn@web.de>
2015-01-15 16:53:37 +0100

    Final Tag4

--- AC_Training/ARothhaupt/AcTraining/Scripts/app/MVVM.js
+++ AC_Training/ARothhaupt/AcTraining/Scripts/app/MVVM.js
@@ -4,18 +4,25 @@
 
     self.header = ko.observable("MVVM!");
     self.customers = ko.observableArray([
-    {
-        Id: ko.observable(0),
-        FirstName: ko.observable("Daten laden..."),
-        LastName: ko.observable("werden geladen..."),
-        DateOfBirth: ko.observable(new Date())
-    }]);
+        {
+            Id: ko.observable(0),
+            FirstName: ko.observable("Daten laden..."),
+            LastName: ko.observable("werden geladen..."),
+            DateOfBirth: ko.observable(new Date())
+        }]);
 
     self.loadData = function () {
+
         $.get('/odata/Customers?$top=10')
-        .success(function (data) {
-            self.customers = ko.mapping.fromJS(data.value, {}, self.customers);
-            console.log(data.value);
-        });
+            .success(function (data) {
+                self.customers = ko.mapping.fromJS(data.value, {}, self.customers);
+            });
+    };
+
+    self.image = function (data) {
+
+        return data && data.Id ?
+            "http://www.gravatar.com/avatar/" + data.Id() + "?d=wavatar&f=y" :
+            "http://www.gravatar.com/avatar/?d=mm&f=y";
     };
 };
