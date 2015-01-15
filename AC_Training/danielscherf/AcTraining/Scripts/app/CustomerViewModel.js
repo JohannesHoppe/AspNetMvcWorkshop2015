2e1f9f7501aa48e2d95d169f62700980eb67b3cd
danielscherf <daniel.scherf.de@gmail.com>
2015-01-15 16:31:38 +0100

    Knockout

--- AC_Training/danielscherf/AcTraining/Scripts/app/CustomerViewModel.js
+++ AC_Training/danielscherf/AcTraining/Scripts/app/CustomerViewModel.js
@@ -1,3 +1,42 @@
-﻿var CustomerViewModel =  function() {
-    this.header = "MVVM!";
+﻿var CustomerViewModel = function () {
+
+    //this.header = ko.observable("MVVM!");
+    //this.color = ko.observable("red");
+    this.hasError = ko.observable(false);
+
+
+    var self = this;
+
+    self.header = ko.observable("MVVM!");
+    self.customers = ko.observableArray([
+        {
+            Id: ko.observable(0),
+            FirstName: ko.observable("Daten laden..."),
+            LastName: ko.observable("werden geladen..."),
+            DateOfBirth: ko.observable(new Date())
+        }]);
+
+    self.loadData = function () {
+
+        $.get('/odata/Customers?$top=10')
+            .success(function (data) {
+                self.customers = ko.mapping.fromJS(data.value, {}, self.customers);
+                console.log(data.value);
+            })
+        .error(function () {
+            self.hasError(true);
+        });
+    };
+
+    self.getImageData = function (customer) {
+        var path = "http://www.gravatar.com/avatar/";
+        var path2 = "?d=wavatar&f=y";
+        var id = customer.Id();
+        var url = path + id + path2;
+        return url;
+    }
+
+    this.activeError = function () {
+        this.hasError(true);
+    }
 }
