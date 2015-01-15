2ce8cb900f5d39eea8592d2b9177843ec8584f82
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-01-15 15:18:58 +0100

    MVVM Klappe die 2

--- AC_Training/Hoppe/AcTraining/Scripts/app/CustomerViewModel.js
+++ AC_Training/Hoppe/AcTraining/Scripts/app/CustomerViewModel.js
@@ -1,14 +1,9 @@
 ﻿var CustomerViewModel = function() {
 
     this.header = ko.observable("MVVM!");
-    this.color = ko.observable("red");
     this.hasError = ko.observable(false);
 
     this.activeError = function() {
         this.hasError(true);
     }
-
-    this.setColor = function () {
-        this.color(this.color() == "green" ? "red" : "green");
-    };
 };
