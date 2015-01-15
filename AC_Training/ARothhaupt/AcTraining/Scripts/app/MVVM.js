68c14be5c7bdced1de54139b83a1270705b1c51b
ArminRothaupt <rotsonn@web.de>
2015-01-15 16:36:48 +0100

    Tag4

---
+++ AC_Training/ARothhaupt/AcTraining/Scripts/app/MVVM.js
@@ -0,0 +1,21 @@
+﻿var CustomerViewModel = function () {
+
+    var self = this;
+
+    self.header = ko.observable("MVVM!");
+    self.customers = ko.observableArray([
+    {
+        Id: ko.observable(0),
+        FirstName: ko.observable("Daten laden..."),
+        LastName: ko.observable("werden geladen..."),
+        DateOfBirth: ko.observable(new Date())
+    }]);
+
+    self.loadData = function () {
+        $.get('/odata/Customers?$top=10')
+        .success(function (data) {
+            self.customers = ko.mapping.fromJS(data.value, {}, self.customers);
+            console.log(data.value);
+        });
+    };
+};
