a2da6e9063cf0ac59b807c7c989a8ed2e040eb00
ArminRothaupt <rotsonn@web.de>
2015-01-16 12:10:56 +0100

    Mittag

---
+++ AC_Training/ARothhaupt/AcTraining/App_Start/Startup.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AcTraining.Starttup))]
+namespace AcTraining
+{
+    public class Starttup
+    {
+        public  void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
